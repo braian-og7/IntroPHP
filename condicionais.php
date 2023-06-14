d250d1121e9a2194f1390e1a4d16eaf7d7777ad8
braian-og7 <braian.pgoulart@senacsp.edu.br>
2023-06-14 11:39:25 -0300

    The Chef

---
+++ condicionais.php
@@ -0,0 +1,22 @@
+<?php
+#CONDICIONAIS
+#TRADICIONAL IF
+$idade = 18;
+if ($idade >= 17)
+{
+    echo("É MAIOR DE IDADE");
+}
+else 
+{
+    echo("É MENOR DE IDADE");
+}
+
+#CONDICIONAL LINDA
+$idade2 = 17;
+echo("<br>");
+echo($idade2 >= 18?"É MAIOR DE IDADE":"MENOR DE IDADE");
+
+
+
+
+?>
---
+++ variaveis.php
@@ -0,0 +1,19 @@
+<?php
+#VARIAVEIS SÃO VARIAVEIS PORQUE VARIAM
+$minhavariavel = "Variavel";
+$numero = 1;
+$decimal = 2.0;
+
+echo("O NOME DA MINHA VARIAVEL É: $minhavariavel<br>");
+echo("O MEU NUMERO é: $numero<br>");
+echo("O MEU NUMERO é: $decimal<br> <br>");
+
+
+#CONCATENAÇÃO DE VARIÁVEIS
+$nome = "BRAIAN";
+$sobrenome = "THE CHEF";
+echo("Meu nome é: ". $nome. " E meu sobrenome é: ".$sobrenome);
+
+echo("Meu nome é: , $nome. E meu sobrenome é:  $sobrenome");
+
+?>
